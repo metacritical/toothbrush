552b98742412c570cea8ee5be1f27bcb79241bc2
Pankaj Doharey <pankajdoharey@gmail.com>
2013-05-22 19:26:51 +0530

    Added tools directory to loader

--- loader.rb
+++ loader.rb
@@ -1,6 +1,10 @@
-["../lib"].each{ |dir| $LOAD_PATH << File.expand_path(dir,__FILE__) }
+["../lib", "../tools"].each{ |dir| $LOAD_PATH << File.expand_path(dir,__FILE__) }
 
 require 'bundler/setup'
 require 'strscan'
+require 'color_printer'
 require 'toothbrush'
 
+#Include Toplevel ToothBrush Module
+include ToothBrush
+include ToothBrush::ColorPrinter
