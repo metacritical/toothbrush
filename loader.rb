2d5ec1e6592d72fdf73390d50a6aa41a1a2905a7
Pankaj Doharey <pankajdoharey@gmail.com>
2013-05-23 15:35:44 +0530

    Colorprinter is required in loader with respect to tools/colorprinter and colorprinter is an independent module now as it does share responsibility with toothbrush module and hence is segregated out

--- loader.rb
+++ loader.rb
@@ -1,10 +1,10 @@
-["../lib", "../tools"].each{ |dir| $LOAD_PATH << File.expand_path(dir,__FILE__) }
+["../", "../lib"].each{ |dir| $LOAD_PATH << File.expand_path(dir,__FILE__) }
 
 require 'bundler/setup'
 require 'strscan'
-require 'color_printer'
+require 'tools/color_printer'
 require 'toothbrush'
 
 #Include Toplevel ToothBrush Module
 include ToothBrush
-include ToothBrush::ColorPrinter
+include ColorPrinter
