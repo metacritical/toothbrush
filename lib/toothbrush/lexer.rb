1e98a6f516273abe06ff1f2571bdaa6691d90651
Pankaj Doharey <pankajdoharey@gmail.com>
2013-05-24 19:00:39 +0530

    Moved lexer switch conditions to some sort of guard clause

--- lib/toothbrush/lexer.rb
+++ lib/toothbrush/lexer.rb
@@ -1,56 +1,38 @@
 module ToothBrush
   class Lexer
-    attr_reader :parsed_tokens , :scanner
+    attr_reader :parsed_tokens , :scanner , :code
     
-    KEYWORDS = %w(if alias)
-
-    ASSIGNMENT = %r|\=|
-
-
     def initialize(input)
       @code = input
       @scanner = StringScanner.new input
       @parsed_tokens = []
     end
-
     def tokenize
       while not @scanner.eos?
-        case
-        when match(/\w+\:/)          then @parsed_tokens << [:METHOD , matched]
-        when match(/\w+/)            then if KEYWORDS.include?(matched)
-                                            @parsed_tokens << [matched.upcase.intern, matched, matched_size]
-                                          else
-                                            @parsed_tokens << [:IDENTIFIER, matched, matched_size]
-                                          end
-        when match(/([ \t]+)/)     then @parsed_tokens << [:WHITESPACE, matched, matched_size]
-        when match(/\s+/) then @parsed_tokens << [:INDENT, matched, matched_size]
-        when match(ASSIGNMENT)       then @parsed_tokens << [:ASSIGNMENT, matched, matched_size]
-        when match(/\'/)             then @parsed_tokens << [:SINGLE_QUOTE, matched, matched_size]
-        when match(/\"/)             then @parsed_tokens << [:DOUBLE_QUOTE, matched, matched_size]
-        when match(/\-\w+/)          then @parsed_tokens << [:PARAMETERS, matched, matched_size]
-        end
+        generate_token
       end
-      @parsed_tokens << [false, false , false]
-      code_inspect @parsed_tokens if ENV["VERBOSE"]
-      @parsed_tokens
+      parsed_tokens << [false, false , false]
+      code_inspect parsed_tokens if ENV["VERBOSE"]
+      parsed_tokens
     end
 
-    private
-    def match regex
-      @scanner.scan regex
-    end
-    
-    def matched
-      @scanner.matched
-    end
-    
-    def matched_size
-      @scanner.matched_size
+    def error!(message)
+      raise LexError, message rescue paint("#{$!.class}\n-> #{$!.message} :: #{getline position}(line)\n", :red);exit
     end
 
-
-
-    def generate_token 
+    def generate_token
+      case
+      when method_token
+      when identifier_token
+      when whitespace_token
+      when indent_token
+      when assignment_token
+      when single_quote_token
+      when double_quote_token
+      when parameters_token
+      else
+        error! "Invalid Token : '#{scanner.check /.*/}' -> #{position}(char)"
+      end
     end
   end
 end
