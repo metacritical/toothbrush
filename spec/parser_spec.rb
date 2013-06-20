374b108d741a052f5057521be36018dca782659b
Pankaj Doharey <pankajdoharey@gmail.com>
2013-06-20 19:15:35 +0530

    Parser Spec updated

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -6,20 +6,25 @@ describe Parser do
   end
 
   it "should out put an ast for following statements" do
-    @parser = parser "alias lsd = 123;" , true
-    @parser = Parser.new
-    @parser.parse "" , true
-    @parser.parse ";" , true
-    @parser.parse "\n" , true
-    @parser.parse "alias" , true
-    @parser.parse "alias;" , true
-    @parser.parse "alias\n" , true
-    @parser.parse "ls;" , true
-    @parser.parse "alias lsd = 'ls -a';" , true
-    @parser.parse "a" , true
-    @parser.parse "a = 1 + 2" , true
-    @parser.parse "a = 3 + 4 + 5 + 6;" , true
+    # @parser = parser "alias lsd = 123;" , true
+     @parser = Parser.new
+    # @parser.parse "" , true
+    # @parser.parse ";" , true
+    # @parser.parse "\n" , true
+    # @parser.parse "alias" , true
+     @parser.parse "alias;" , true
+    # @parser.parse "alias\n" , true
+    # @parser.parse "ls;" , true
+    # @parser.parse "alias lsd = 'ls -a';" , true
+    # @parser.parse "a" , true
+    # @parser.parse "a = 1 + 2" , true
+    # @parser.parse "a = 3 + 4 + 5 + 6;" , true
     @parser.parse "A=123" , true
+    @parser.parse %Q{
+hello:
+  echo "hi world!"
+} , true
+
   end
 end
 
